f2061cfbe04b6e6c8aaa50363004988a174fd2f7
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-09 19:35:24 +1100

    HTTP Error page

---
+++ src/routes/_error.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let status;
+  export let error;
+
+  const dev = process.env.NODE_ENV === "development";
+</script>
+
+<style>
+  #container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  h1,
+  p {
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>{status}</title>
+</svelte:head>
+
+<div id="container" class="has-text-centered">
+  <h1>{status}</h1>
+
+  <p>{error.message}</p>
+
+  {#if dev && error.stack}
+    <pre>{error.stack}</pre>
+  {/if}
+</div>
