6f7b158e1ab92007a0fbab7378b0df0637a20779
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-09 18:11:19 +1100

    Splash page

---
+++ src/components/splashPage/SWEC_iframe.svelte
@@ -0,0 +1,45 @@
+<style>
+  /* .thumbnail-container {
+    width: 300px;
+    height: 200px;
+} */
+  iframe {
+    max-width: initial;
+  }
+
+  .thumbnail-container {
+    width: 320px !important;
+    height: 180px !important;
+  }
+
+  .thumbnail iframe {
+    width: 1280px;
+    height: 720px;
+    border-radius: 30px;
+    border: none;
+
+    -ms-zoom: 0.25;
+    -moz-transform: scale(0.25);
+    -moz-transform-origin: 0 0;
+    -o-transform: scale(0.25);
+    -o-transform-origin: 0 0;
+    -webkit-transform: scale(0.25);
+    -webkit-transform-origin: 0 0;
+  }
+
+  .thumbnail:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+</style>
+
+<div class="thumbnail-container">
+  <div class="thumbnail">
+    <iframe src="https://swec.org.au" />
+  </div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Banner from "../components/splashPage/Banner.svelte";
+  import SWECiframe from "../components/splashPage/SWEC_iframe.svelte";
+</script>
+
+<style>
+  #mainContent {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  #contentPanel {
+    width: 500px;
+    margin: 0 auto;
+  }
+
+  #pageContainer {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  #pageContainer > section:first-child {
+    flex: 1;
+  }
+
+  .bannerContainer {
+    margin-bottom: 200px;
+  }
+</style>
+
+<div id="pageContainer">
+  <section id="mainContent">
+    <div class="level" id="contentPanel">
+      <div class="level-left">
+        <div class="level-item">
+          <SWECiframe />
+        </div>
+      </div>
+
+      <div class="level-right">
+        <div class="level-item">
+          <aside class="menu">
+            <ul class="menu-list">
+              <li>
+                <a href="portal/">
+                  <span class="icon">
+                    <i class="fas fa-tools" />
+                  </span>
+                  Manage
+                </a>
+              </li>
+              <li>
+                <a href="https://swec.org.au">
+                  <span class="icon">
+                    <i class="fas fa-home" />
+                  </span>
+                  Go to SWEC
+                </a>
+              </li>
+              <li>
+                <a>
+                  <span class="icon">
+                    <i class="fas fa-file-alt" />
+                  </span>
+                  View
+                </a>
+              </li>
+            </ul>
+          </aside>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="bannerContainer">
+    <Banner />
+  </div>
+</div>
