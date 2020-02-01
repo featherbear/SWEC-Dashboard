0667b30115a8bec9cfae83ea9c00844c976952d9
Andrew Wong <andrew.j.wong@outlook.com>
2020-02-01 17:02:18 +1100

    Elvanto provider and grant

---
+++ plugins/elvanto-grant/middlewares/elvanto-grant/index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/**
+ * Module dependencies
+ */
+
+// Public node modules.
+const _ = require('lodash')
+let a = require('grant')
+
+module.exports = strapi => {
+  return {
+    async initialize () {
+      let grantStore = await strapi.store({
+        environment: '',
+        type: 'plugin',
+        name: 'users-permissions',
+        key: 'grant'
+      })
+
+      let data = await grantStore.get()
+      if (!data['elvanto']) return
+
+      data['elvanto'] = {
+        ...data['elvanto'],
+        ...{
+          authorize_url: 'https://api.elvanto.com/oauth',
+          access_url: 'https://api.elvanto.com/oauth/token',
+          oauth: 2,
+          scope: ['ManagePeople', 'ManageCalendar'],
+          custom_params: {
+            type: 'web_server'
+          }
+        }
+      }
+
+      await grantStore.set({ key: 'grant', value: data })
+
+      // console.log("Injected grant:elvanto");
+    }
+  }
+}
