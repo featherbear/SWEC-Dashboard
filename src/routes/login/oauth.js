529c831e68ec879743b55be365709d4b629fe482
Andrew Wong <andrew.j.wong@outlook.com>
2020-02-07 00:45:34 +1100

    Elvanto -> Strapi implementation

--- src/routes/login/oauth.js
+++ src/routes/login/oauth.js
@@ -1,18 +1,47 @@
 const redirect = require('@polka/redirect')
-
+const fetch = require('node-fetch')
 const elvanto = require('elvanto-api')
 
 export async function get (req, res, next) {
-  const { elvantoClientID, elvantoClientSecret, elvantoOAuthReturn } = process.env
+  const {
+    elvantoClientID,
+    elvantoClientSecret,
+    elvantoOAuthReturn
+  } = process.env
 
-  if (req.query.code) {
-    // Get Elvanto UID
-    let { accessToken } = await elvanto.exchangeToken(elvantoClientID, elvantoClientSecret, req.query.code, elvantoOAuthReturn)
-    let client = new elvanto.Client({ accessToken })
-    let data = await client.apiCall('people/currentUser')
+  if (req.query['code']) {
+    let data = await fetch(
+      `http://localhost:1337/connect/elvanto/callback?code=${req.query.code}`,
+      {
+        credentials: 'include',
+        headers: {
+          cookie: req.headers.cookie
+        }
+      }
+    ).then(r => r.json())
 
-    res.end(data['person'][0]['id'])
+    console.log(data)
+
+    if (data.error) {
+      res.write(JSON.stringify(data))
+      res.statusCode = data.statusCode
+      return res.end()
+    }
+
+    res.cookie('jwtToken', data.jwt, {
+      path: '/'
+    })
+    res.cookie('user', JSON.stringify(data.user), {
+      path: '/'
+    })
+    
+    res.redirect('/')
+
+    res.end()
   } else {
-    return redirect(res, elvanto.authorizeUrl(elvantoClientID, elvantoOAuthReturn, ['ManagePeople', 'ManageCalendar']))
+    return redirect(
+      res,
+      'http://localhost:1337/connect/elvanto?redirect_uri=http%3A%2F%2Flocalhost:3000/login/oauth'
+    )
   }
 }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import polka from 'polka'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 
+import { json } from 'body-parser'
+import cookierParser from 'cookie-parser'
+
 // import dotenv from 'dotenv'
 
 // import core from './backend/core'
@@ -13,8 +16,13 @@ import * as sapper from '@sapper/server'
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
 
+import './components/ServerResponse-CookiePolyfill'
+import './components/ServerResponse-RedirectPolyfill'
+
 polka() // You can also use Express
   .use(
+    json(),
+    cookierParser(),
     compression({ threshold: 0 }),
     sirv('static', { dev }),
     sapper.middleware()
