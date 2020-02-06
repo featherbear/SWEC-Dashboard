e42ebbc76ca5e4fe35d59d5ee05b19e2af7ba382
Andrew Wong <andrew.j.wong@outlook.com>
2020-02-07 01:11:50 +1100

    Add sapper session auth middleware

---
+++ src/routes/portal/index.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export async function preload(page, session) {
+    const fail = () => this.redirect(302, "/login");
+    console.log(session);
+    if (!session) return fail();
+
+    // return { user };
+  }
+</script>
+
+<script>
+  export let user;
+</script>
+
+<svelte:head>
+  <title>SWEC Dashboard</title>
+</svelte:head>
--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import * as sapper from '@sapper/server'
 import { json } from 'body-parser'
 import cookierParser from 'cookie-parser'
 
+import jwt_decode from 'jwt-decode'
+
 // import dotenv from 'dotenv'
 
 // import core from './backend/core'
@@ -25,7 +27,24 @@ polka() // You can also use Express
     cookierParser(),
     compression({ threshold: 0 }),
     sirv('static', { dev }),
-    sapper.middleware()
+    sapper.middleware({
+      session: (req, res) => {
+        let data = null
+
+        try {
+          data = jwt_decode(req.cookies.token)
+          data.token = req.cookies.token
+          
+          if (data.exp < Date.now() / 1000) {
+            data = null
+          }
+        } catch (e) {
+          data = null
+        }
+
+        return data
+      }
+    })
   )
   .listen(PORT, err => {
     if (err) console.log('error', err)
