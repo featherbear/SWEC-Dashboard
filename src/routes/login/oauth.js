693428b731a61aa0d699d3fa4b0afba412ba1ff3
Andrew Wong <andrew.j.wong@outlook.com>
2020-05-21 00:52:02 +1000

    Begin migration to internal OAuth provider.
    api.elvanto.com/oauth/token doesn't work with application/json POST bodies

--- src/routes/login/oauth.js
+++ src/routes/login/oauth.js
@@ -4,44 +4,48 @@ const elvanto = require('elvanto-api')
 
 export async function get (req, res, next) {
   const {
-    elvantoClientID,
-    elvantoClientSecret,
-    elvantoOAuthReturn
+    ELVANTO_CLIENT_ID,
+    ELVANTO_CLIENT_SECRET,
+    ELVANTO_OAUTH_RETURN
   } = process.env
 
-  if (req.query['code']) {
-    let data = await fetch(
-      `http://localhost:1337/connect/elvanto/callback?code=${req.query.code}`,
-      {
-        credentials: 'include',
-        headers: {
-          cookie: req.headers.cookie
-        }
-      }
-    ).then(r => r.json())
-
-    console.log(data)
-
-    if (data.error) {
-      res.write(JSON.stringify(data))
-      res.statusCode = data.statusCode
-      return res.end()
-    }
-
-    res.cookie('jwtToken', data.jwt, {
-      path: '/'
-    })
-    res.cookie('user', JSON.stringify(data.user), {
-      path: '/'
-    })
-    
-    res.redirect('/')
-
-    res.end()
-  } else {
+  if (!req.query['code']) {
     return redirect(
       res,
-      'http://localhost:1337/connect/elvanto?redirect_uri=http%3A%2F%2Flocalhost:3000/login/oauth'
+      `https://api.elvanto.com/oauth?type=web_server&client_id=${ELVANTO_CLIENT_ID}&redirect_uri=${ELVANTO_OAUTH_RETURN}&scope=ManagePeople,ManageCalendar`
     )
   }
+
+  console.log(
+    JSON.stringify({
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      client_id: ELVANTO_CLIENT_ID,
+      client_secret: ELVANTO_CLIENT_SECRET,
+      redirect_uri: ELVANTO_OAUTH_RETURN
+    })
+  )
+
+  let data = await fetch('http://api.elvanto.com/oauth/token', {
+    method: 'POST',
+    body: `grant_type=authorization_code&client_id=${ELVANTO_CLIENT_ID}&client_secret=${ELVANTO_CLIENT_SECRET}&code=${req.query.code}&redirect_uri=${ELVANTO_OAUTH_RETURN}`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+    .then(r => r.json())
+
+  if (data.error) {
+    res.write(JSON.stringify(data))
+    res.statusCode = data.statusCode
+    return res.end()
+  }
+
+  // res.cookie('token', data.jwt, {
+  //   path: '/'
+  // })
+
+  res.redirect('/')
+
+  res.end()
 }
