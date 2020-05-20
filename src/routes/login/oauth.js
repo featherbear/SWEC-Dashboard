b7f40dad6a480500b129927d9d9647df1b7d8fcc
Andrew Wong <andrew.j.wong@outlook.com>
2020-05-21 01:05:37 +1000

    currentUser handle

--- src/routes/login/oauth.js
+++ src/routes/login/oauth.js
@@ -32,8 +32,7 @@ export async function get (req, res, next) {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
-  })
-    .then(r => r.json())
+  }).then(r => r.json())
 
   if (data.error) {
     res.write(JSON.stringify(data))
@@ -41,11 +40,30 @@ export async function get (req, res, next) {
     return res.end()
   }
 
-  // res.cookie('token', data.jwt, {
-  //   path: '/'
-  // })
+  let { access_token } = data
+
+  let userInfo = await fetch(
+    'https://api.elvanto.com/v1/people/currentUser.json',
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
+    }
+  ).then(r => r.json())
+
+  if (userInfo.status === 'ok') {
+    let user = userInfo.person[0];
+    // user.status;
+    
+    // TODO: connect with mongo db --- user.id
+    // (user.preferred_name || user.firstname);
+    // user.lastname || "";
+
+    // res.cookie('token', data.jwt, {
+    //   path: '/'
+    // })
+  }
 
   res.redirect('/')
-
   res.end()
 }
