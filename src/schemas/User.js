db505c07a554bb72bde76760d67f35efed56aae4
Andrew Wong <andrew.j.wong@outlook.com>
2020-05-22 16:47:39 +1000

    Username requirement for User

--- src/schemas/User.js
+++ src/schemas/User.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import mongoose_bcrypt from 'mongoose-bcrypt'
 
 let User = new mongoose.Schema({
-  username: { type: String, unique: true, lowercase: true, trim: true },
+  username: { type: String, required: true, unique: true, lowercase: true, trim: true },
   firstName: { type: String, trim: true },
   lastName: { type: String, trim: true },
   isLocal: {type: Boolean, default: false},
