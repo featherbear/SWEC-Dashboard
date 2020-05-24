6e387f8482ab08d46491d4e7fad288786ff30b3f
Andrew Wong <andrew.j.wong@outlook.com>
2020-05-25 00:56:08 +1000

    Show empty data

--- src/routes/portal/notices/index.svelte
+++ src/routes/portal/notices/index.svelte
@@ -1,15 +1,21 @@
 <script context="module">
   export async function preload({ params, query }, session) {
-    return { data: await this.fetch("http://localhost:3000/api/notices").then(r=>r.json()) };
+    return {
+      data: await this.fetch("http://localhost:3000/api/notices").then(r =>
+        r.json()
+      )
+    };
   }
 </script>
 
 <script>
-  export let data;
+  export let data = [];
 </script>
 
 <ul>
-  {#each data as entry}
-    <li>{entry.title} - {entry.description}</li>
-  {/each}
+  {#if Object.keys(data).length != 0}
+    {#each data as entry}
+      <li>{entry.title} - {entry.description}</li>
+    {/each}
+  {:else}No data!{/if}
 </ul>
