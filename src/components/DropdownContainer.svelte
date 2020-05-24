4d5c7c8b35a6f9b1ef1fc5e9ca1d0d6a48e49bcf
Andrew Wong <andrew.j.wong@outlook.com>
2020-05-25 02:57:49 +1000

    Use Svelma Button

--- src/components/DropdownContainer.svelte
+++ src/components/DropdownContainer.svelte
@@ -1,16 +1,18 @@
 <script>
   export let hoverable = true;
   export let label = "Dropdown";
+
+  import Button from "svelma/src/components/Button.svelte";
 </script>
 
 <div class="dropdown" class:is-hoverable={hoverable}>
   <div class="dropdown-trigger">
-    <button class="button">
+    <Button>
       <span>{label}</span>
       <span class="icon is-small">
         <i class="fas fa-angle-down" />
       </span>
-    </button>
+    </Button>
   </div>
   <div class="dropdown-menu">
     <div class="dropdown-content">
